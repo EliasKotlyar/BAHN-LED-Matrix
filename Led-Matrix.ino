fcb4508ec867d8870de1d3c75c99b962bc04fcdc
Elias Kotlyar <twinsen23@gmail.com>
2016-04-21 18:58:42 +0200

    New Version

--- Led-Matrix.ino
+++ Led-Matrix.ino
@@ -1,152 +1,171 @@
-int latchPin = 10;  // Rck Pin
-int dataPin = 9;   // SERPin
-int clockPin = 8;  // SerClk Pin
-int powerPin = 2;
+int latchPin = 12;  // Rck Pin / pin 12
+int dataPin = 11;   // SERPin / pin 11 (MOSI pin)
+int clockPin = 13;  // SerClk Pin / pin 13 (SCK pin)
 int A2PIN = 7;
 int A1PIN = 6;
 int A0PIN = 5;
 int A0Value;
 int A1Value;
 int A2Value;
-int lineNr = 0;
-#include "TimerOne.h"  
+byte lineNr = 0;
+#include "TimerOne.h"
+//#include "digitalWrite.h"
+#include <SPI.h>
+byte displayMatrix[17][7];
 
+SPISettings settingsA(16000000, MSBFIRST, SPI_MODE0);
 /**
- * Sets a Row According to the Set
- * http://stackoverflow.com/questions/8695945/c-get-a-bit-from-a-byte
- */
-void setRow(int rowNr){
-   // int A0Value = (rowNr >> 0)  & 0x01;
-   // int A1Value = (rowNr >> 1)  & 0x01;
-   // int A2Value = (rowNr >> 2)  & 0x01;
-    
-    
+   Sets a Row According to the Set
+   http://stackoverflow.com/questions/8695945/c-get-a-bit-from-a-byte
+*/
+void setRow(int rowNr) {
+  switch (rowNr) {
+    case 0:
+      A0Value = 0;
+      A1Value = 0;
+      A2Value = 0;
+      break;
+    case 1:
+      A0Value = 0;
+      A1Value = 1;
+      A2Value = 0;
+      break;
+    case 2:
+      A0Value = 0;
+      A1Value = 0;
+      A2Value = 1;
+      break;
+    case 3:
+      A0Value = 1;
+      A1Value = 1;
+      A2Value = 0;
+      break;
+    case 4:
+      A0Value = 0;
+      A1Value = 1;
+      A2Value = 1;
+      break;
+    case 5:
+      A0Value = 1;
+      A1Value = 0;
+      A2Value = 1;
+      break;
+    case 6:
+      A0Value = 1;
+      A1Value = 1;
+      A2Value = 1;
+      break;
+    case 7:
+      A0Value = 1;
+      A1Value = 0;
+      A2Value = 0;
+      break;
+  }
+  digitalWrite(A0PIN, A0Value);
+  digitalWrite(A1PIN, A1Value);
+  digitalWrite(A2PIN, A2Value);
 
-    //digitalWrite(A0PIN,A0Value);
-    //digitalWrite(A1PIN, A1Value);
-    //digitalWrite(A2PIN, A2Value);
-
-    switch(rowNr){
-      case 0:
-       A0Value = 0;
-       A1Value = 0;
-       A2Value = 0;
-       break;
-       case 1:
-       A0Value = 0;
-       A1Value = 1;
-       A2Value = 0;
-       break;
-       case 2:
-       A0Value = 0;
-       A1Value = 0;
-       A2Value = 1;
-       break;
-       case 3:
-       A0Value = 1;
-       A1Value = 1;
-       A2Value = 0;
-       break;
-       case 4:
-       A0Value = 0;
-       A1Value = 1;
-       A2Value = 1;
-       break;
-       case 5:
-       A0Value = 1;
-       A1Value = 0;
-       A2Value = 1;
-       break;
-       case 6:
-       A0Value = 1;
-       A1Value = 1;
-       A2Value = 1;
-       break;
-       case 7:
-       A0Value = 1;
-       A1Value = 0;
-       A2Value = 0;
-       break;
-    }
-    digitalWrite(A0PIN, A0Value);
-    digitalWrite(A1PIN, A1Value);
-    digitalWrite(A2PIN, A2Value);
-    /*
-
-    Serial.print("A0:");
-    Serial.println(A0Value);
-    Serial.print("A1:");
-    Serial.println(A1Value);
-    Serial.print("A2:");
-    Serial.println(A2Value);
-    */
 }
 void setup() {
-   //Serial.begin(9600);
+  Serial.begin(9600);
   byte pinArray[] = {
     latchPin,
     clockPin,
     dataPin,
-    powerPin,
     A0PIN,
     A1PIN,
-    A2PIN,3
+    A2PIN
   };
   for (int i = 0; i < sizeof(pinArray); i++) {
     //Serial.println(pinArray[i]);
-    
+
     pinMode(pinArray[i], OUTPUT);
     digitalWrite(pinArray[i], LOW);
   }
-  //digitalWrite(A2PIN,HIGH);
-  //digitalWrite(A1PIN,HIGH);
-  //digitalWrite(A0PIN,HIGH);
-  digitalWrite(powerPin, HIGH);     
-    delay(1000);
-        Timer1.initialize(1000000);
-    Timer1.attachInterrupt(umschalten);
-}
-
-void sendByte(int b) {
-  shiftOut(dataPin, clockPin, MSBFIRST, b);
-  // shift out the bits:
-
-}
+  
+  
 
 
-void sendLine(){
-  for(int number = 0; number < 17; number ++){
-      sendByte(number);
+
+  for (byte number = 0; number < 17; number ++) {
+    for(byte line = 0; line < 8; line ++){
+      displayMatrix[number][line] = 0;
     }
-    //delay(25);     
-    digitalWrite(latchPin, HIGH);
-    //delayMicroseconds(5);
-    digitalWrite(latchPin, LOW);   
+    
+  }
+
+  Timer1.initialize(500);
+  Timer1.attachInterrupt(umschalten);
+
+
 }
-void umschalten(){
+
+
+
+
+
+
+void umschalten() {
+
+  setRow(lineNr);
+  SPI.begin();
+  SPI.beginTransaction(settingsA);
+
+  // Send Line:
+  for (byte number = 0; number < 17; number++) {
+    byte transferbuffer = displayMatrix[number][lineNr];
+    //transferbuffer = 255;
+    //transferbuffer = 0;
+    SPI.transfer(transferbuffer);
    
-   setRow(lineNr);
-   sendLine();
-   lineNr++;
-   if(lineNr == 8 ){
-     lineNr = 0;
-   }
+    Serial.print("LEDNR:");
+    Serial.print(number);
+    Serial.print(" LineNR");
+    Serial.print(lineNr);
+    Serial.println();
+
+    Serial.println(transferbuffer);
+    //delay(10000);
+
+  }
+  SPI.endTransaction();
+  SPI.end();
+
+  digitalWrite(latchPin, HIGH);
+  delayMicroseconds(300);
+  digitalWrite(latchPin, LOW);
+  
+  //SPI.endTransaction();
+  // Increase Line Number
+
+
+  lineNr++;
+  if (lineNr == 8 ) {
+    lineNr = 0;
+  }
 }
 void loop() {
+  
+  delay(1000);
+  //displayMatrix[number][lineNr]
+  //umschalten();
+  //  delay(500);
+
+  //delayMicroseconds(500);
 
 
-               
-   
-
-     
 
 
-    
 
 
-    
+
+
+
+
+
 
 
 }
+
 
 
